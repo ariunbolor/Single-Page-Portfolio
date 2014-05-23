58ba49e856b4157730aa0c86790e5c31f83d6d75
Bolorerdene Bundgaa <bolor@ariunbolor.org>
2014-05-23 17:42:28 -0400

    Todo
    committing  soon

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ sppApp.config(function($routeProvider){
 	.when('/', {templateUrl: 'partials/home.html', controller: 'mainController'})
 	.when('/blank', {templateUrl: 'partials/blog.html', controller: 'aboutController'})
 	.when('/casestudy', {templateUrl: 'partials/carousel.html', controller: 'caseController'})
+	.when('/todo', {templateUrl: 'partials/todo.html', controller: 'todoController'});
 	.when('/contact', {templateUrl: 'partials/page.html', controller: 'aboutController'});
 });
+
